679186361798d5a90a4ef5664b954844850df73f
Денис Ковалев <iliroted@gmail.com>
2023-10-08 13:18:23 +0300

    Hello_world_on_other_language

---
+++ HELLo_2.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var lang := ReadInteger('Введите свой язык (1 - русский, 2 - англ, 3 - испанский');
+  Assert(lang in 1..3);
+  if lang = 1 then
+    Println('Привет мир!');
+  if lang = 2 then
+    Println('Hello world!');
+  if lang = 3 then
+     Println('Halló heimur!');
+end.
